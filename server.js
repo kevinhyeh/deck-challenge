14eed3eaaef378b9779ceef319323319dbcd9563
Kevin Yeh <kevinyeh@Kevins-MBP-3.attlocal.net>
2018-10-27 15:15:34 -0700

    added routes to server.js

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser());
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "deck_db"
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
+
+app.get('/users', (req, res) => {
+  connection.query('SELECT * FROM users', function(err, results) {
+    if (err) throw err;
+    console.log(results);
+  });
+});
+
+app.post('/signup', (req, res) => {
+  // console.log(req.body.name)
+  let user = req.body.user;
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
+  connection.query("INSERT INTO users (user, email, username, password) VALUES (?,?,?,?)", [user, email, username, password]);
+});
+
+app.post('/workouts', (req, res) => {
+  connection.query("SELECT * FROM workouts", function(err, data) {
+    res.json(data);
+  });
+});
+
+app.listen(3001, () => {
+    console.log("listening on 3001");
+});
